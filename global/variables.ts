ba97b8fc01ab1fd892f86c70e0819441238e8f7c
Mario Krstevski <mariokrstevski@hotmail.com>
2024-01-10 22:16:08 +0100

    duco deploy

---
+++ global/variables.ts
@@ -0,0 +1,5 @@
+const defaultStoreId = "8aacb4bd-6b25-4500-aa2f-71eaa77e9abe"; // Suzana Kroevi
+const defaultBillboardId = "8324be14-61c1-4281-b96e-2045e56c84cf"; // Vencanici
+
+export const storeId = "ab760feb-432e-456b-8901-a5f5f04d0ba9"; //bokicmoki
+export const billboardId = "3668be89-46b6-48c3-8943-ee1ded31c0a3"; //boki board
