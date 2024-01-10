abb78cecf73f45f6732d8745f7b5cc1b5c9bfee3
Mario Krstevski <mariokrstevski@hotmail.com>
2024-01-10 22:19:09 +0100

    duco config variables

--- global/variables.ts
+++ global/variables.ts
@@ -1,5 +1,5 @@
 const defaultStoreId = "8aacb4bd-6b25-4500-aa2f-71eaa77e9abe"; // Suzana Kroevi
 const defaultBillboardId = "8324be14-61c1-4281-b96e-2045e56c84cf"; // Vencanici
 
-export const storeId = "ab760feb-432e-456b-8901-a5f5f04d0ba9"; //bokicmoki
-export const billboardId = "3668be89-46b6-48c3-8943-ee1ded31c0a3"; //boki board
+export const storeId = "6ef36afb-904b-473d-bce6-05b84d695339"; //Fitness
+export const billboardId = "b49f4b07-bfac-4648-8fe0-21206815e3cd"; //protein shake
